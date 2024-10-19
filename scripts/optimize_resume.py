8267de35e4456cf2c68ef8eb23763bce7488abd6
Kevin Jägle <95292087+kevinjaegle@users.noreply.github.com>
2024-10-19 19:11:46 +0200

    Update optimize_resume.py

--- scripts/optimize_resume.py
+++ scripts/optimize_resume.py
@@ -16,13 +16,7 @@ else:
 
     # Lebenslauf und Stellenbeschreibung definieren
     md_resume = """
-    # Kevin Jägle
-
-    **Berufsbezeichnung:** DevOps Engineer  
-    **E-Mail:** kevinjaegle@outlook.de  
-    **Telefon:** +49 15758178224  
-    **Adresse:** 77716 Fischerbach  
-
+    
     ## Über Mich
     Junior DevOps Engineer mit Erfahrung in Containerisierung, Automatisierung und Cloud-Technologien. Spezialisiert auf CI/CD-Pipelines und Blockchain-Technologien.
 
